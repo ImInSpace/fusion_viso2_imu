30630e3bde4b1fedfc103b3598b1d626806e7d2e
ImInSpace <ignigomoreno@gmail.com>
2020-08-31 13:41:33 +0200

    Added file that was missing from git for ellipse drawing

---
+++ draw_ellipse.m
@@ -0,0 +1,9 @@
+function ellipse = draw_ellipse(X,Q)
+    [V,D]=eig(Q);
+    radii=sqrt(diag(D));
+    th=linspace(0,2*pi,20);
+    x=radii(1)*sin(th);
+    y=radii(2)*cos(th);
+    P=X(:)+V*[x;y];
+    ellipse = polyshape(P(1,:),P(2,:));
+end
