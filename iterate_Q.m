f6451e7a07321323bf9a700e9961710d684cc841
ImInSpace <ignigomoreno@gmail.com>
2020-09-07 12:43:05 +0200

    Added a matlab file that tests different values for Q and checks for drift.

---
+++ iterate_Q.m
@@ -0,0 +1,35 @@
+close all
+N=100;
+Qms=0.05*10.^linspace(-1,1.5,N);
+kerror=zeros(1,N);
+
+figure()
+hold on
+for i=1:N
+    disp(i);
+    addpath(genpath('third-party/yamlmatlab'))
+    yamls=yaml.ReadYaml('config.yaml');
+    yamls{3}.Qm=Qms(i);
+    yaml.WriteYaml('config2.yaml',yamls);
+    system('cmake-build-debug\test_fusion_viso2_imu.exe 3 0 config2.yaml');
+    X=csvread("data.csv");
+    tx=X(:,1); ty=X(:,2);
+    ox=X(:,3); oy=X(:,4);
+    kx=X(:,5); ky=X(:,6);
+    scatter(tx(1),ty(1),30,'r');
+    plot(tx,ty,'r')
+    plot(ox,oy,'g')
+    plot(kx,ky,'b')
+    drawnow
+    Pk=reshape(X(:,end-3:end).',[2 2 size(X,1)]);
+
+    dist_traveled=[0;cumsum(sqrt(diff(tx).^2+diff(ty).^2))];
+    dist_oerror=vecnorm([ox-tx oy-ty],2,2);
+    dist_kerror=vecnorm([kx-tx ky-ty],2,2);
+    dlmo = fitlm(dist_traveled,dist_oerror,'Intercept',false);
+    dlmk = fitlm(dist_traveled,dist_kerror,'Intercept',false);
+    oe=dlmo.Coefficients.Estimate*100;
+    ke=dlmk.Coefficients.Estimate*100;
+    fprintf('%.1f%%\n',ke/oe*100);
+    kerror(i)=ke/oe*100;
+end
